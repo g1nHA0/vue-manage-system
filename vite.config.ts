15b1921664d6469d85a48b2f00aa6d80e2d2d4c5
g1nHA0 <czhtzsj@gmail.com>
2023-02-23 17:03:28 +0800

    修改 添加路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,39 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import VueSetupExtend from 'vite-plugin-vue-setup-extend';
-import AutoImport from 'unplugin-auto-import/vite';
-import Components from 'unplugin-vue-components/vite';
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import path from "path";
+import vue from "@vitejs/plugin-vue";
+import VueSetupExtend from "vite-plugin-vue-setup-extend";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 export default defineConfig({
-	base: './',
-	plugins: [
-		vue(),
-		VueSetupExtend(),
-		AutoImport({
-			resolvers: [ElementPlusResolver()]
-		}),
-		Components({
-			resolvers: [ElementPlusResolver()]
-		})
-	],
-	optimizeDeps: {
-		include: ['schart.js']
-	}
+  base: "./",
+  plugins: [
+    vue(),
+    VueSetupExtend(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  resolve: {
+    // ↓路径别名，主要是这部分
+    alias: {
+      "@": path.join(__dirname, "./src"),
+      "@components": path.join(__dirname, "./src/components"),
+      "@utils": path.join(__dirname, "./src/utils"),
+      "@router": path.join(__dirname, "./src/router"),
+      "@request": path.join(__dirname, "./src/utils/request"),
+      "@store": path.join(__dirname, "./src/store"),
+      "@storage": path.join(__dirname, "./src/utils/storage"),
+      "@model": path.join(__dirname, "./src/utils/model"),
+      "@service": path.join(__dirname, "./src/api/service"),
+      "@handler": path.join(__dirname, "./src/api/handler"),
+    },
+  },
+  optimizeDeps: {
+    include: ["schart.js"],
+  },
 });
