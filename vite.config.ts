2fa9c3d121ada80ec7230000e6fd1deb35236b1c
g1nHA0 <czhtzsj@gmail.com>
2023-03-14 10:39:34 +0800

    修改 修改vite设置的base

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 import path from "path";
 import vue from "@vitejs/plugin-vue";
 import VueSetupExtend from "vite-plugin-vue-setup-extend";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
 export default defineConfig({
-  base: "./",
+  base: "/vue-manage-system/",
   plugins: [
     vue(),
     VueSetupExtend(),
