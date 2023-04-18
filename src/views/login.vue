5a3f4f903dca4c543cd40de2133ca968281b83a5
g1nHA0 <czhtzsj@gmail.com>
2023-04-18 20:55:44 +0800

    SpringSecurity临时提交

--- src/views/login.vue
+++ src/views/login.vue
@@ -31,7 +31,6 @@
         <div class="login-btn">
           <el-button type="primary" @click="submitForm(login)">登录</el-button>
         </div>
-        <p class="login-tips">Tips : 用户名和密码随便填。</p>
       </el-form>
     </div>
   </div>
